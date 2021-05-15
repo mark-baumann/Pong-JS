9e94f6106b10d21dc70ddab15d7b7adefd345d86
Mark Baumann <60445235+mark-baumann@users.noreply.github.com>
2021-05-15 22:05:48 +0200

    Change the Paddle

--- pong.js
+++ pong.js
@@ -67,6 +67,74 @@ function drawBall(x, y, radius, color) {
   ctx.fill();
 }
 
-//eigenschaften des objektes an die Funktion übergeben
- drawBall(ball.x, ball.y, ball.radius, ball.color);
- drawPaddle(user.x, user.y, user.width, user.height, user.color);
+
+window.addEventListener('keydown', keyDownHandler);
+window.addEventListener('keyup', keyUpHandler);
+
+function keyDownHandler(event) {
+  // get the keyCode
+  switch (event.keyCode) {
+    // "up arrow" key
+    case 38:
+      // set upArrowPressed = true
+      upArrowPressed = true;
+      break;
+    // "down arrow" key
+    case 40:
+      downArrowPressed = true;
+      break;
+  }
+}
+
+// gets activated when we release the key
+function keyUpHandler(event) {
+  switch (event.keyCode) {
+    // "up arraow" key
+    case 38:
+      upArrowPressed = false;
+      break;
+    // "down arrow" key
+    case 40:
+      downArrowPressed = false;
+      break;
+  }
+}
+
+
+function updateBall() {
+  ball.x += ball.velocityX;
+  ball.y += ball.velocityY;
+
+//logik das sich das Paddle bewegt
+  if (upArrowPressed && user.y > 0) {
+    user.y -= 8;
+  } else if (downArrowPressed && (user.y < canvas.height - user.height)) {
+    user.y += 8;
+  }
+
+  //Logik das der Ball oben wieder abdotzt
+  if (ball.y + ball.radius >= canvas.height || ball.y - ball.radius <= 0) {
+
+
+  ball.velocityY = -ball.velocityY;
+}
+
+
+//rechts Wand
+if (ball.x + ball.radius >= canvas.width) {
+ }
+
+ if (ball.x - ball.radius <= 0) {
+  }
+
+
+}
+
+function gameLoop() {
+  drawBall(ball.x, ball.y, ball.radius, ball.color);
+  drawPaddle(user.x, user.y, user.width, user.height, user.color);
+
+  updateBall();
+}
+
+setInterval(gameLoop, 100 / 60);
